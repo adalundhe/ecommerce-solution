af0c826c865563f67ff090a581cc2daff5c997cb
fresh5447 <doug@kosmojo.com>
2017-09-07 12:54:59 -0600

    upgrade functions

--- server/routes/Users/auth.js
+++ server/routes/Users/auth.js
@@ -1,19 +1,19 @@
-const User = require('../../models/User')
+require('../../models/User')
 
-module.exports = function(app, passport){
+module.exports = (app, passport) => {
 
 //****** SIGN UP ********
-  app.post('/api/signup', function(req, res, next){
+  app.post('/api/signup', (req, res, next) => {
     console.log('FOUND SIGNUP ROUTE');
-    const authenticator = passport.authenticate('local-signup', function(err, user, info){
+    const authenticator = passport.authenticate('local-signup', (err, user, info) => {
       if(err) return next(err); //res.send sends back anything you want (not just raw json specific data)
       if(!user){
         return res.status(404).json(info.message) //"user name already taken" AND "blank username"
       } else {
-        user.save(function(err){
+        user.save((err) => {
           if(err) throw err;  //this is a mongoose error (this stops the program from working)
           console.log("USER SUCCESSFULLY SAVED");
-          req.logIn(user, function(err){
+          req.logIn(user,(err) => {
             if(err) res.json({error: err.message});
             return res.json(user);
           })
@@ -25,7 +25,7 @@ module.exports = function(app, passport){
 
 
   //***** GET USER ********
-    app.get('/api/get_user', function(req, res, next){ //endpoint makes more sense as "get_user" rather than "login"
+    app.get('/api/get_user', (req, res, next) => { //endpoint makes more sense as "get_user" rather than "login"
       if(req.user){ //only exists (part of the req object) if someone has successfully logged in
         console.log('user IS logged in');
         res.json({user: req.user, isAuthed: true}) //if the user is in, show them
@@ -39,14 +39,14 @@ module.exports = function(app, passport){
     });
 
     //****** LOG IN (SIGN IN)*******
-      app.post('/api/login', function(req, res, next){
+      app.post('/api/login',(req, res, next) => {
         //this is another way of passing in a required module its arguments for a given method
-        const authenticator = passport.authenticate('local-login', function(err, user, info){
+        const authenticator = passport.authenticate('local-login', (err, user, info) => {
           if(err) res.json({error: err.message});
           if (!user){
             return res.status(404).json(info.message); //info is an object
           }
-          req.logIn(user, function(err){
+          req.logIn(user,(err) => {
             if(err) res.json({error: err.message}); //uses json to send back error to client
             console.log(user);
             return res.json({
@@ -62,9 +62,9 @@ module.exports = function(app, passport){
 
   //****** LOG OUT *******
     //every route we create has to be inside this module.exports function
-    app.get('/api/logout', function (req, res, next){ //do this one first
+    app.get('/api/logout', (req, res, next) => { //do this one first
       req.session.destroy();
-      return res.json({//this is a method!
+      return res.json({
         loggedOut: 'Successfully logged out! Bravo!'
       })
     });
