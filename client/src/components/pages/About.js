8398a6e679d8cde5de37dd203c084e3044cfc264
fresh5447 <doug@kosmojo.com>
2018-01-05 15:06:54 -0700

    update home and about pages to use material ui

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import injectSheet from 'react-jss'
+import PropTypes from 'prop-types'
+import Typography from 'material-ui/Typography'
+// https://material-ui-next.com/style/typography/
+import Card from 'material-ui/Card'
 
-const About = () =>
-  <div>
-    <h1>About</h1>
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20
+  },
+  contentContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 20
+  },
+  subheader: {
+    color: '#8e44ad !important'
+  }
+}
+
+const enhancer = injectSheet(styles)
+
+const About = ({classes}) =>
+  <div className={classes.container}>
+    <Card>
+      <div className={classes.contentContainer}>
+        <Typography type='display1' gutterBottom className={classes.subheader}>
+          Mock e-commerce project built for Big Sky Code Academy
+        </Typography>
+      </div>
+    </Card>
   </div>
 
-export default About
+About.propTypes = {
+  classes: PropTypes.object
+}
+
+export default enhancer(About)
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,8 +1,46 @@
 import React from 'react'
+import injectSheet from 'react-jss'
+import PropTypes from 'prop-types'
+import Typography from 'material-ui/Typography'
+// https://material-ui-next.com/style/typography/
+import Card from 'material-ui/Card'
 
-const Home = () =>
-  <div>
-    <h1>Home</h1>
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20
+  },
+  contentContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 20
+  },
+  subheader: {
+    color: '#8e44ad !important'
+  }
+}
+
+const enhancer = injectSheet(styles)
+
+const Home = ({classes}) =>
+  <div className={classes.container}>
+    <Card>
+      <div className={classes.contentContainer}>
+        <Typography type='display4' gutterBottom>
+        Zamazon
+        </Typography>
+        <Typography type='display1' gutterBottom className={classes.subheader}>
+        Amazon - with a ⚡️
+        </Typography>
+      </div>
+    </Card>
   </div>
 
-export default Home
+Home.propTypes = {
+  classes: PropTypes.object
+}
+
+export default enhancer(Home)
